b54fb3a20fac9c4051f8123a1754fde92d3ba09d
Alin <alin794@hotmail.com>
2014-06-12 19:54:10 +0300

    Added Agent n CommonFunctions

---
+++ Agent.h
@@ -0,0 +1,14 @@
+#include "Date.h"
+#include "Time.h"
+#include <string>
+using namespace std;
+
+class Agent public:CommonFunctions
+{
+public:
+	int Id;
+	string Name;
+	Agent();
+	~Agent();
+	
+};
---
+++ CommonFunctions.h
@@ -0,0 +1,15 @@
+#include "Date.h"
+#include "Time.h"
+#include <string>
+using namespace std;
+
+class CommonFunctions
+{
+public:
+	void SearchTicket();
+	void BookTicket();
+	void CancelTicket();
+	void MakePayment();
+	void FillDetails();
+	
+};
